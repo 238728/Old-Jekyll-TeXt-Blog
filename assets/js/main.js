1cad3544b50a20a1d221f14e6ee01cf06d31d120
238728 <102637209+238728@users.noreply.github.com>
2023-01-04 08:45:51 +0800

    bloggg

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,58 +1,46 @@
-// Fix DOM matches function
-if (!Element.prototype.matches) {
-  Element.prototype.matches =
-    Element.prototype.matchesSelector ||
-    Element.prototype.mozMatchesSelector ||
-    Element.prototype.msMatchesSelector ||
-    Element.prototype.oMatchesSelector ||
-    Element.prototype.webkitMatchesSelector ||
-    function(s) {
-      var matches = (this.document || this.ownerDocument).querySelectorAll(s),
-        i = matches.length;
-      while (--i >= 0 && matches.item(i) !== this) {}
-      return i > -1;
-    };
-}
+$(function () {
+  // Cache variables for increased performance on devices with slow CPUs.
+  var flexContainer = $('div.flex-container')
+  var searchBox = $('.search-box')
+  var searchClose = $('.search-icon-close')
+  var searchInput = $('#search-input')
 
-// Get Scroll position
-function getScrollPos() {
-  var supportPageOffset = window.pageXOffset !== undefined;
-  var isCSS1Compat = ((document.compatMode || "") === "CSS1Compat");
+  // Menu Settings
+  $('.menu-icon, .menu-icon-close').click(function (e) {
+    e.preventDefault()
+    e.stopPropagation()
+    flexContainer.toggleClass('active')
+  })
 
-  var x = supportPageOffset ? window.pageXOffset : isCSS1Compat ? document.documentElement.scrollLeft : document.body.scrollLeft;
-  var y = supportPageOffset ? window.pageYOffset : isCSS1Compat ? document.documentElement.scrollTop : document.body.scrollTop;
+  // Click outside of menu to close it
+  flexContainer.click(function (e) {
+    if (flexContainer.hasClass('active') && e.target.tagName !== 'A') {
+      flexContainer.removeClass('active')
+    }
+  })
 
-  return { x: x, y: y };
-}
+  // Press Escape key to close menu
+  $(window).keydown(function (e) {
+    if (e.key === 'Escape') {
+      if (flexContainer.hasClass('active')) {
+        flexContainer.removeClass('active')
+      } else if (searchBox.hasClass('search-active')) {
+        searchBox.removeClass('search-active')
+      }
+    }
+  })
 
-var _scrollTimer = [];
+  // Search Settings
+  $('.search-icon').click(function (e) {
+    e.preventDefault()
+    searchBox.toggleClass('search-active')
+    searchInput.focus()
 
-// Smooth scroll
-function smoothScrollTo(y, time) {
-  time = time == undefined ? 500 : time;
-
-  var scrollPos = getScrollPos();
-  var count = 60;
-  var length = (y - scrollPos.y);
-
-  function easeInOut(k) {
-    return .5 * (Math.sin((k - .5) * Math.PI) + 1);
-  }
-
-  for (var i = _scrollTimer.length - 1; i >= 0; i--) {
-    clearTimeout(_scrollTimer[i]);
-  }
-
-  for (var i = 0; i <= count; i++) {
-    (function() {
-      var cur = i;
-      _scrollTimer[cur] = setTimeout(function() {
-        window.scrollTo(
-          scrollPos.x,
-          scrollPos.y + length * easeInOut(cur/count)
-        );
-      }, (time / count) * cur);
-    })();
-  }
-}
-
+    if (searchBox.hasClass('search-active')) {
+      searchClose.click(function (e) {
+    		e.preventDefault()
+    		searchBox.removeClass('search-active')
+    	})
+    }
+  })
+})
